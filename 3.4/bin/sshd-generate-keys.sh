4617dbe58c024707dff0d211c7ff2da6f57fbda9
Chingis Sandanov <chingis@wodby.com>
2017-09-28 14:19:03 +0700

    Fix script

--- 3.4/bin/sshd-generate-keys.sh
+++ 3.4/bin/sshd-generate-keys.sh
@@ -13,5 +13,5 @@ ssh-keygen -qb 2048 -t rsa -N "" -f "${dir}/ssh_host_rsa_key"
 ssh-keygen -qt dsa -N '' -f "${dir}/ssh_host_dsa_key"
 ssh-keygen -qt ecdsa -N '' -f "${dir}/ssh_host_ecdsa_key"
 ssh-keygen -qt ed25519 -N '' -f "${dir}/ssh_host_ed25519_key"
-chmod 0600 "${dir}/*_key"
-chmod 0644 "${dir}/*.pub"
+chmod 0600 "${dir}"/*_key
+chmod 0644 "${dir}"/*.pub
