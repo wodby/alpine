e30929572f3784564a8558be3e04722dd09826cb
Chingis Sandanov <chingis@wodby.com>
2018-07-03 12:37:47 +0700

    clean up

--- test.sh
+++ test.sh
@@ -12,57 +12,57 @@ run() {
     docker run --rm -e DEBUG "${IMAGE}" "${@}"
 }
 
-#echo "START TESTING get_archive"
-#echo
-#
-## Extension to test :: Allowed extensions
-#test_cases=(
-#    'zip::'
-#    'tar.gz::gz tgz tar.gz'
-#    'tgz::tgz gz'
-#    'sql.gz::gz tar.gz'
-#    'tar::tar.gz tar'
-#)
-#
-#for index in "${test_cases[@]}" ; do
-#    ext="${index%%::*}"
-#    allowed="${index##*::}"
-#    dest="/tmp/${ext}/"
-#    url="${s3url}.${ext}"
-#    file="/tmp/export.${ext}"
-#
-#    echo "Testing ${ext} archive with allowed extensions: ${allowed}"
-#    echo -n "From URL source... "
-#    run sh -c "get_archive ${url} ${dest} ${allowed} && [[ -e ${dest}/export.sql ]]"
-#    echo "OK"
-#
-#    echo -n "From file source... "
-#    source="/tmp/export.${ext}"
-#    run sh -c "wget ${url} -qP /tmp && get_archive ${file} ${dest} ${allowed} && [[ -e ${dest}/export.sql ]]"
-#    echo "OK"
-#done
-#
-#echo -n "Checking errors: missing params... "
-#run sh -c "! get_archive" | grep -iq "missing params"
-#echo "OK"
-#
-#echo -n "Checking errors: unsupported archive... "
-#run sh -c "! get_archive ${s3url} /tmp/dir/ 'tmp tar.gz zip'" | grep -iq "unsupported archive"
-#echo "OK"
-#
-#echo
-#echo "END TESTING get_archive"
-#
-#run sh -c '[[ $(compare_semver "4.3.6" "4.3.5") == 0 ]];'
-#run sh -c '[[ $(compare_semver "3.3.6" "4.3.5") == 1 ]];'
-#run sh -c '[[ $(compare_semver "3.3.6" "4.3.5" "<") == 0 ]];'
-#run sh -c '[[ $(compare_semver "3.3.6" "3.3.6" "=") == 0 ]];'
-#
-#run sh -c "apk add --update openssh-keygen || apk add --update openssh-client;
-#           gen_ssh_keys && rm /etc/ssh/ssh_rsa_key*;
-#           gen_ssh_keys 'rsa dsa ecdsa ed25519'"
-#
-#run sh -c "apk add --update libressl || apk add --update openssl; gen_ssl_certs /tmp wodby.com test 365"
+echo "START TESTING get_archive"
+echo
+
+# Extension to test :: Allowed extensions
+test_cases=(
+    'zip::'
+    'tar.gz::gz tgz tar.gz'
+    'tgz::tgz gz'
+    'sql.gz::gz tar.gz'
+    'tar::tar.gz tar'
+)
+
+for index in "${test_cases[@]}" ; do
+    ext="${index%%::*}"
+    allowed="${index##*::}"
+    dest="/tmp/${ext}/"
+    url="${s3url}.${ext}"
+    file="/tmp/export.${ext}"
+
+    echo "Testing ${ext} archive with allowed extensions: ${allowed}"
+    echo -n "From URL source... "
+    run sh -c "get_archive ${url} ${dest} ${allowed} && [[ -e ${dest}/export.sql ]]"
+    echo "OK"
+
+    echo -n "From file source... "
+    source="/tmp/export.${ext}"
+    run sh -c "wget ${url} -qP /tmp && get_archive ${file} ${dest} ${allowed} && [[ -e ${dest}/export.sql ]]"
+    echo "OK"
+done
+
+echo -n "Checking errors: missing params... "
+run sh -c "! get_archive" | grep -iq "missing params"
+echo "OK"
+
+echo -n "Checking errors: unsupported archive... "
+run sh -c "! get_archive ${s3url} /tmp/dir/ 'tmp tar.gz zip'" | grep -iq "unsupported archive"
+echo "OK"
+
+echo
+echo "END TESTING get_archive"
+
+run sh -c '[[ $(compare_semver "4.3.6" "4.3.5") == 0 ]];'
+run sh -c '[[ $(compare_semver "3.3.6" "4.3.5") == 1 ]];'
+run sh -c '[[ $(compare_semver "3.3.6" "4.3.5" "<") == 0 ]];'
+run sh -c '[[ $(compare_semver "3.3.6" "3.3.6" "=") == 0 ]];'
+
+run sh -c "apk add --update openssh-keygen || apk add --update openssh-client;
+           gen_ssh_keys && rm /etc/ssh/ssh_rsa_key*;
+           gen_ssh_keys 'rsa dsa ecdsa ed25519'"
+
+run sh -c "apk add --update libressl || apk add --update openssl; gen_ssl_certs /tmp wodby.com test 365"
 
 run sh -c "cd /tmp;
            curl -fSL https://nginx.org/download/nginx-1.12.2.tar.gz -o nginx.tar.gz;
